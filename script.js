ac5039a7622cae373ff3ae8788244901b0f14e4d
BasPennings <152502925+BasPennings@users.noreply.github.com>
2024-04-17 14:44:53 +0200

    Styled the navigation bar

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+    Idea language switching
+    - in HTML add lang param to text.
+    - Create text in nl and en, also add lang for each tag.
+    - Set display to none for lang(nl) if language selector
+      is on en.
+    - Set display to none for lang(en) if language selector
+      is on nl.
+*/
